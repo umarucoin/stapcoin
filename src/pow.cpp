5914b31887974a458f8842469e8311114cea2ff7
macks <macks77@hotmail.com>
2017-09-24 23:25:16 +0900

    second commit

--- src/pow.cpp
+++ src/pow.cpp
@@ -251,11 +251,11 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&
 
     // Check range
     if (fNegative || bnTarget == 0 || fOverflow || bnTarget > UintToArith256(params.powLimit))
-        return error("CheckProofOfWork(): nBits below minimum work");
+        return false;//error("CheckProofOfWork(): nBits below minimum work");
 
     // Check proof of work matches claimed amount
     if (UintToArith256(hash) > bnTarget)
-        return error("CheckProofOfWork(): hash doesn't match nBits");
+        return false;//error("");//("CheckProofOfWork(): hash doesn't match nBits");
 
     return true;
 }
